24e0f0cc9fb87fb201458d47aa3cf93f1d7fceec
Martin Jovanovski <martin.jovanovski@students.finki.ukim.mk>
2026-06-22 19:34:54 +0200

    Initial backend commit

---
+++ backend/src/main/java/org/example/crtachbackend/CrtachBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.crtachbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrtachBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrtachBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/example/crtachbackend/CrtachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crtachbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrtachBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
