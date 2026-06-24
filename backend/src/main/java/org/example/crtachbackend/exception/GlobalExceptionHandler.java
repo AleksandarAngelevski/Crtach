812161173e12532615bb7b2ba97fa95d6b46b218
Martin Jovanovski <martin.jovanovski@students.finki.ukim.mk>
2026-06-24 19:21:41 +0200

    [CRTAC-4-create-user-repository] Backend changes: *[CRTAC-4-create-user-repository] Created the WebSecurityConfig.java . *[CRTAC-4-create-user-repository] Created the GlobalExceptionHandler.java . *[CRTAC-4-create-user-repository] Added mapstruct to pom.xml .

---
+++ backend/src/main/java/org/example/crtachbackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package org.example.crtachbackend.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * A Controller advice class used
+ * for catching all exceptions
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * Exception handler method that
+     * handles the entity not found
+     * exception
+     *
+     * @param e - entity not found exception
+     *          parameter
+     *
+     * @return - returns a new response entity
+     *          with a status code of not found
+     *          and exception message
+     */
+    @ExceptionHandler(value={EntityNotFoundException.class})
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e){
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Exception handler method that
+     * handles the illegal argument
+     * exception
+     *
+     * @param e - the illegal argument exception
+     *          parameter
+     *
+     * @return - returns a new response entity
+     *          with a status code of bad
+     *          request and exception message
+     */
+    @ExceptionHandler(value={IllegalArgumentException.class})
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e){
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
