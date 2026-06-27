712a66d1dee4560274503293b2f6e3a74948428b
Martin Jovanovski <watsum@hotmail.com>
2026-06-27 23:00:09 +0200

    [CRTAC-8-add-user-login] Minor backend changes: *[CRTAC-8-add-user-login] Created handleUsernameNotFound in GlobalExceptionHandler.java . *[CRTAC-8-add-user-login] Some minor changes in WebSecurityConfig.java . *[CRTAC-8-add-user-login] Version changes  for jwt in pom.xml .

--- backend/src/main/java/org/example/crtachbackend/security/WebSecurityConfig.java
+++ backend/src/main/java/org/example/crtachbackend/security/WebSecurityConfig.java
@@ -1,11 +1,15 @@
 package org.example.crtachbackend.security;
 
+import org.example.crtachbackend.config.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,15 +23,34 @@ import java.util.List;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter  jwtAuthenticationFilter;
+
+    public WebSecurityConfig(AuthenticationProvider authenticationProvider, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.cors(cors -> {
-                }).
-                authorizeHttpRequests((requests) ->
-                        requests.requestMatchers("/**", "/api", "/api/**").permitAll()).csrf(AbstractHttpConfigurer::disable);
+
+        httpSecurity
+                .cors(cors -> cors.configurationSource(corsConfiguration()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
+
 
     @Bean
     public CorsConfigurationSource corsConfiguration() {
